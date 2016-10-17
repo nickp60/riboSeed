56a73010e5db008ce88f6dd44d2b3060681e623d
nickp60 <nickp60@gmail.com>
2016-10-17 17:58:40 +0100

    tested up through convert bams

--- tests/test_riboSeed.py
+++ tests/test_riboSeed.py
@@ -39,7 +39,7 @@ logger = logging
 from pyutilsnrw.utils3_5 import make_output_prefix, check_installed_tools,\
     copy_file, get_ave_read_len_from_fastq, get_number_mapped,\
     extract_mapped_and_mappedmates, keep_only_first_contig, md5,\
-    combine_contigs, clean_temp_dir, md5
+    combine_contigs, clean_temp_dir, file_len
 
 from riboSeed.riboSeed import  check_smalt_full_install,\
     map_to_ref_smalt, convert_bams_to_fastq, estimate_distances_smalt
@@ -52,24 +52,27 @@ from riboSeed.riboSeed import  check_smalt_full_install,\
 class utils3_5TestCase(unittest.TestCase):
     def setUp(self):
         self.test_dir = os.path.join(os.path.dirname(__file__),
-                                        "output_riboseed_tests")
+                                     "output_riboseed_tests")
         self.spades_dir = os.path.join(os.path.dirname(__file__),
-                                        "output_riboseed_tests",
+                                       "output_riboseed_tests",
                                        "SPAdes_results")
         self.ref_dir = os.path.join(os.path.dirname(__file__), "references")
         self.ref_fasta = os.path.join(self.ref_dir,
-                                       'cluster1.fasta')
+                                      'cluster1.fasta')
         self.ref_Ffastq = os.path.join(self.ref_dir,
-                                        'toy_reads1.fq')
+                                       'toy_reads1.fq')
         self.ref_Rfastq = os.path.join(self.ref_dir,
-                                        'toy_reads2.fq')
+                                       'toy_reads2.fq')
+        self.ref_bam_prefix = os.path.join(self.ref_dir,
+                                       'test_bam_to_fastq')
         self.smalt_exe = "smalt"
+        self.samtools_exe = "samtools"
         self.test_estimation_file = os.path.join(self.test_dir,
                                                  "est_distance.sam")
         self.map_results_prefix = os.path.join(self.test_dir,
                                                "test_mapping")
         self.fastq_results_prefix = os.path.join(self.test_dir,
-                                                 "test_mapping_fastq")
+                                                 "test_bam_to_fastq")
         # self.ref_disctance
         # self.pileup
         pass
@@ -79,22 +82,36 @@ class utils3_5TestCase(unittest.TestCase):
             os.makedirs(self.test_dir)
         self.assertTrue(os.path.exists(self.test_dir))
 
-
     def test_estimate_distances_smalt(self):
-        # test_estimation_file = os.path.join(self.test_dir,
-        #                                     "est_distance.sam")
         if os.path.exists(self.test_estimation_file):
             print("warning! existing distance esimation file!")
-
         est_file = estimate_distances_smalt(outfile=self.test_estimation_file,
                                             smalt_exe=self.smalt_exe,
                                             ref_genome=self.ref_fasta,
                                             fastq1=self.ref_Ffastq,
                                             fastq2=self.ref_Rfastq,
                                             cores=1,
-                                            logger=None)
+                                            logger=logger)
+        ref_smi_md5 = "a444ccbcb486a8af29736028640e87cf"  # determined manually
+        ref_sma_md5 = "4ce0c8b453f2bdabd73eaf8b5ee4f376"  # determined manually
+        ref_mapping_len = 9271  # mapping doesnt have exact order, so cant md5
+        self.assertEqual(ref_smi_md5, md5(str(est_file + ".smi")))
+        self.assertEqual(ref_sma_md5, md5(str(est_file + ".sma")))
+        self.assertEqual(ref_mapping_len, file_len(est_file))
+
+    def test_convert_bams_to_fastq(self):
+        if not os.path.exists(self.fastq_results_prefix):
+            os.makedirs(self.fastq_results_prefix)
+        convert_bams_to_fastq(map_results_prefix=self.ref_bam_prefix,
+                              fastq_results_prefix=self.fastq_results_prefix,
+                              keep_unmapped=False,
+                              samtools_exe=self.samtools_exe)
 
     def test_map_to_ref_smalt(self):
+        """ this tests that the resulting bam files when converted to
+        sam has the right number of lines.  Not that helpful, open to
+        other ideas
+        """
         est_file = estimate_distances_smalt(outfile=self.test_estimation_file,
                                             smalt_exe=self.smalt_exe,
                                             ref_genome=self.ref_fasta,
@@ -109,49 +126,55 @@ class utils3_5TestCase(unittest.TestCase):
                          map_results_prefix=self.map_results_prefix,
                          cores=1, samtools_exe="samtools",
                          smalt_exe="smalt", fastq_readS="",
-                         read_len=100, step=3, k=5,
+                         read_len=100, step=10, k=20,
+                         score_minimum=40,
                          scoring="match=1,subst=-4,gapopen=-4,gapext=-3",
                          logger=logger)
-        ####
-        #TODO make actual tests;  best way to do that?
+        wc_cmds = ["samtools view -S {0} |wc".format(self.map_results_prefix + "_pe.bam")]
+        wc_res = subprocess.run(wc_cmds, shell=sys.platform != "win32",
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE, check=True)
+        # lines and words determinied manually.
+        # Bytes change depending on cline call
+        lines_and_words = ["36350","", "472550"]  #wc returns "lines\swords\s\sbytes
+        for i in range(0, 3):
+            self.assertEqual(wc_res.stdout.decode("utf-8").strip().split(" ")[i],
+                             lines_and_words[i])
 
-    def test_convert_bams_to_fastq(self):
-        convert_bams_to_fastq(self.map_results_prefix,
-                              self.fastq_results_prefix,
-                              keep_unmapped=False)
 
-    def test_run_spades(self):
-        spades_success = run_spades(output=self.spades_dir,
-                                    ref=self.ref_fasta,
-                                    ref_as_contig="trusted",
-                                    pe1_1=self.ref_Ffasta,
-                                    pe1_2=self.ref_Rfasta, pe1_s='',
-                                    as_paired=True, keep_best=True,
-                                    prelim=False,
-                                    groom_contigs='keep_first',
-                                    k="21,33,55", seqname='',
-                                    spades_exe="spades.py")
 
-    def test_check_samtools_pileup(self):
-        check_samtools_pileup(self.pileup)
+    # def test_run_spades(self):
+    #     spades_success = run_spades(output=self.spades_dir,
+    #                                 ref=self.ref_fasta,
+    #                                 ref_as_contig="trusted",
+    #                                 pe1_1=self.ref_Ffasta,
+    #                                 pe1_2=self.ref_Rfasta, pe1_s='',
+    #                                 as_paired=True, keep_best=True,
+    #                                 prelim=False,
+    #                                 groom_contigs='keep_first',
+    #                                 k="21,33,55", seqname='',
+    #                                 spades_exe="spades.py")
 
-    def test_reconstruct_seq(self):
-        reconstruct_seq(refpath, pileup, verbose=True, veryverb=False,
-                    logger=None)
+    # def test_check_samtools_pileup(self):
+    #     check_samtools_pileup(self.pileup)
+
+    # def test_reconstruct_seq(self):
+    #     reconstruct_seq(refpath, pileup, verbose=True, veryverb=False,
+    #                     logger=None)
 
     def tearDown(self):
-        # test_estimation_file = os.path.join(self.test_dir,
-        #                                     "est_distance.sam")
-        if os.path.exists(self.test_estimation_file):
-            print("removing test distance estimation file")
-            os.remove(self.test_estimation_file)
+        # # test_estimation_file = os.path.join(self.test_dir,
+        # #                                     "est_distance.sam")
+        # if os.path.exists(self.test_estimation_file):
+        #     print("removing test distance estimation file")
+        #     os.remove(self.test_estimation_file)
         pass
 
 if __name__ == '__main__':
     tools_needed = ["samtools", "smalt", "spades.py"]
     for i in tools_needed:
-        if not check_installed_tools(smalt_exe, hard=False):
-            print("Error! Can't run without install of {0} in PATH".format(i)))
+        if not check_installed_tools(i, hard=False):
+            print("Error! Can't run without install of {0} in PATH".format(i))
             sys.exit(1)
-    logger=logging
+    logger = logging
     unittest.main()
