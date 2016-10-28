5255685a96af1ec701edc5b83fd586dcc355f926
nickp60 <nickp60@gmail.com>
2016-10-28 15:07:10 +0100

    midway though integration testing

--- tests/test_riboSnag.py
+++ tests/test_riboSnag.py
@@ -100,13 +100,13 @@ class riboSnag_TestCase(unittest.TestCase):
         self.assertEqual(clusters[0].sequence, ref_seq_name)
         self.assertEqual(test_locus_tags, ref_loci_list)
 
-    # def test_genbank_match_id(self):
-    #     """ tests get_genbank_rec_from_multigb
-    #     """
-    #     records = get_genbank_record(self.test_gb_file)
-    #     record = get_genbank_rec_from_multigb(recordID='NC_011751.1',
-    #                                           genbank_records=records)
-    #     self.assertEqual(records[0].seq, record.seq)
+    def test_genbank_match_id(self):
+        """ tests get_genbank_rec_from_multigb
+        """
+        records = get_genbank_record(self.test_gb_file)
+        record = get_genbank_rec_from_multigb(recordID='NC_011751.1',
+                                              genbank_records=records)
+        self.assertEqual(records[0].seq, record.seq)
 
     def test_extract_coords_from_locus(self):
         """todo: replace essentially unused get_genbank_record function
@@ -165,42 +165,43 @@ class riboSnag_TestCase(unittest.TestCase):
                     x in cluster_post_extract.loci_list]
         padded_cluster = pad_genbank_sequence(cluster_post_extract,
                                               logger=logger)
+        self.assertEqual(old_seq.seq,
+                         padded_cluster.SeqRecord.seq[padding_val: -padding_val])
 
-    # def test_strictly_increasing(self):
-    #     """ I pulled this largely from SO, and it should check to
-    #     see if items are in an increasing order, with and without duplicates
-    #     """
-    #     self.assertTrue(strictly_increasing([1, 5, 5.5, 10, 10], dup_ok=True))
-    #     with self.assertRaises(ValueError):
-    #         strictly_increasing([1, 5, 5.5, 10, 10], dup_ok=False)
-    #     self.assertFalse(strictly_increasing([1, 10, 5.5, 7, 9], dup_ok=False,
-    #                                          verbose=False))
+    def test_strictly_increasing(self):
+        """ I pulled this largely from SO, and it should check to
+        see if items are in an increasing order, with and without duplicates
+        """
+        self.assertTrue(strictly_increasing([1, 5, 5.5, 10, 10], dup_ok=True))
+        with self.assertRaises(ValueError):
+            strictly_increasing([1, 5, 5.5, 10, 10], dup_ok=False)
+        self.assertFalse(strictly_increasing([1, 10, 5.5, 7, 9], dup_ok=False,
+                                             verbose=False))
 
-    # def test_stitching(self):
-    #     """  This is actually the thing needing the most testing, most likely
-    #     """
-    #     records = get_genbank_record(self.test_gb_file)
-    #     clusters = parse_clustered_loci_file(self.test_loci_file,
-    #                                          logger=logger)
-    #     record = get_genbank_rec_from_multigb(recordID='NC_011751.1',
-    #                                           genbank_records=records)
-    #     coord_list = extract_coords_from_locus(record=record,
-    #                                            locus_tag_list=clusters[0][1],
-    #                                            feature="rRNA",
-    #                                            logger=logger)
-    #     stitched_record = \
-    #         stitch_together_target_regions(genome_sequence=record.seq,
-    #                                        coords=coord_list,
-    #                                        flanking="700:700",
-    #                                        within=50, minimum=50,
-    #                                        replace=False,
-    #                                        padding=0,  # unused
-    #                                        logger=logger,
-    #                                        verbose=False)
-    #     with open(self.test_cluster1, 'r') as ref:
-    #         ref_rec = list(SeqIO.parse(ref, 'fasta'))[0]
-    #     self.assertEqual(ref_rec.seq, stitched_record.seq)
-    #     # If reimplementing replacement write test cases here
+    def test_stitching(self):
+        """  This is actually the thing needing the most testing, most likely
+        """
+        padding_val = 50
+        clusters = parse_clustered_loci_file(self.test_loci_file,
+                                             padding=100,
+                                             circular=True,
+                                             logger=logger)
+        cluster = clusters[0]
+        cluster.SeqRecord = get_genbank_rec_from_multigb(recordID=cluster.
+                                                         genbank_records=records)
+        cluster_post_extract = extract_coords_from_locus(cluster=cluster,
+                                                         feature="CDS",
+                                                         logger=logger)
+        stitched_cluster = \
+            stitch_together_target_regions(cluster=cluster_post_extract,
+                                           flanking="500",
+                                           within=50, minimum=50, replace=True,
+                                           logger=None, verbose=False, circular=False,
+                                           revcomp=False)
+        with open(self.test_cluster1, 'r') as ref:
+            ref_rec = list(SeqIO.parse(ref, 'fasta'))[0]
+        self.assertEqual(ref_rec.seq, stitched_cluster.extractedSeqRecord.seq)
+        # If reimplementing replacement write test cases here
 
     # def test_stitching_integration(self):
     #     """  Integration of several things
@@ -249,87 +250,87 @@ class riboSnag_TestCase(unittest.TestCase):
     #     # coords were accuratly adjusted
     #     self.assertEqual(stitched_record.seq, stitched_padded_record.seq)
 
-    # def test_calc_Shannon(self):
-    #     """ test calc_shannon_entropy;
-    #     may need to recalculate
-    #     """
-    #     test_matrix = [
-    #         ["A", "A", "A", "A", "A"],
-    #         ["A", "A", "A", "A", "G"],
-    #         ["A", "A", "A", "G", "G"],
-    #         ["A", "A", "G", "G", "G"],
-    #         ["A", "T", "G", "G", "G"],
-    #         ["T", "T", "G", "G", "G"],
-    #         ["A", "C", "G", "T", "-"]
-    #     ]
-    #     entropies = calc_Shannon_entropy(test_matrix)
-    #     print(entropies)
-    #     theoretical = [-0.0, 0.500402, 0.673012, 0.673012,
-    #                    0.950271, 0.673012, 1.609438]
-    #     for index, value in enumerate(entropies):
-    #         self.assertEqual(round(value, 6), theoretical[index])
+    def test_calc_Shannon(self):
+        """ test calc_shannon_entropy;
+        may need to recalculate
+        """
+        test_matrix = [
+            ["A", "A", "A", "A", "A"],
+            ["A", "A", "A", "A", "G"],
+            ["A", "A", "A", "G", "G"],
+            ["A", "A", "G", "G", "G"],
+            ["A", "T", "G", "G", "G"],
+            ["T", "T", "G", "G", "G"],
+            ["A", "C", "G", "T", "-"]
+        ]
+        entropies = calc_Shannon_entropy(test_matrix)
+        print(entropies)
+        theoretical = [-0.0, 0.500402, 0.673012, 0.673012,
+                       0.950271, 0.673012, 1.609438]
+        for index, value in enumerate(entropies):
+            self.assertEqual(round(value, 6), theoretical[index])
 
-    # def test_calc_entropy_from_msa(self):
-    #     """
-    #     """
-    #     # calc_Shannon_entropy(msa_path=self.test_prank_msa)
-    #     values = calc_entropy_msa(msa_path=self.test_mafft_msa)
-    #     with open(self.test_ent_seq, "r") as efile:
-    #         for index, line in enumerate(efile):
-    #             print(line)
-    #             print(line.strip())
-    #             self.assertEqual(round(float(line.strip()), 7),
-    #                              round(values[index], 7))
+    def test_calc_entropy_from_msa(self):
+        """
+        """
+        # calc_Shannon_entropy(msa_path=self.test_prank_msa)
+        values = calc_entropy_msa(msa_path=self.test_mafft_msa)
+        with open(self.test_ent_seq, "r") as efile:
+            for index, line in enumerate(efile):
+                print(line)
+                print(line.strip())
+                self.assertEqual(round(float(line.strip()), 7),
+                                 round(values[index], 7))
 
-    # def test_prepare_msa_cmds(self):
-    #     """ test cases of prank and mafft command creation
-    #     """
-    #     for dir in [self.testdirname, self.test_snag_dir]:
-    #         try:
-    #             os.makedirs(self.testdirname)
-    #         except:
-    #             print("using existing {0} directory".format(dir))
-    #             pass
-    #     unaligned_seqs = combine_contigs(contigs_dir=self.test_snag_dir,
-    #                                      pattern="*",
-    #                                      contigs_name="riboSnag_unaligned",
-    #                                      ext=".fasta", verbose=False,
-    #                                      logger=logger)
-    #     prank_cmd_1, results_path = prepare_prank_cmd(
-    #         outdir=self.testdirname,
-    #         outfile_name="best_MSA.fasta",
-    #         combined_fastas=unaligned_seqs,
-    #         prank_exe=self.prank_exe,
-    #         add_args="-t=sometree",
-    #         clobber=False, logger=None)
-    #     mafft_cmd_1, results_path = prepare_mafft_cmd(
-    #         outdir=self.testdirname,
-    #         outfile_name="best_MSA.fasta",
-    #         combined_fastas=unaligned_seqs,
-    #         mafft_exe=self.mafft_exe,
-    #         add_args="-t=sometree",
-    #         clobber=False, logger=None)
-    #     idealprank = "prank -t=sometree -d={0} -o={1}".format(
-    #         unaligned_seqs, os.path.join(self.testdirname,
-    #                                      "best_MSA.fasta"))
-    #     idealmafft = "mafft -t=sometree {0} > {1}".format(
-    #         unaligned_seqs, os.path.join(self.testdirname,
-    #                                      "best_MSA.fasta"))
-    #     self.assertEqual(idealprank, prank_cmd_1)
-    #     self.assertEqual(idealmafft, mafft_cmd_1)
-    #     self.to_be_removed.append(unaligned_seqs)
+    def test_prepare_msa_cmds(self):
+        """ test cases of prank and mafft command creation
+        """
+        for dir in [self.testdirname, self.test_snag_dir]:
+            try:
+                os.makedirs(self.testdirname)
+            except:
+                print("using existing {0} directory".format(dir))
+                pass
+        unaligned_seqs = combine_contigs(contigs_dir=self.test_snag_dir,
+                                         pattern="*",
+                                         contigs_name="riboSnag_unaligned",
+                                         ext=".fasta", verbose=False,
+                                         logger=logger)
+        prank_cmd_1, results_path = prepare_prank_cmd(
+            outdir=self.testdirname,
+            outfile_name="best_MSA.fasta",
+            combined_fastas=unaligned_seqs,
+            prank_exe=self.prank_exe,
+            add_args="-t=sometree",
+            clobber=False, logger=None)
+        mafft_cmd_1, results_path = prepare_mafft_cmd(
+            outdir=self.testdirname,
+            outfile_name="best_MSA.fasta",
+            combined_fastas=unaligned_seqs,
+            mafft_exe=self.mafft_exe,
+            add_args="-t=sometree",
+            clobber=False, logger=None)
+        idealprank = "prank -t=sometree -d={0} -o={1}".format(
+            unaligned_seqs, os.path.join(self.testdirname,
+                                         "best_MSA.fasta"))
+        idealmafft = "mafft -t=sometree {0} > {1}".format(
+            unaligned_seqs, os.path.join(self.testdirname,
+                                         "best_MSA.fasta"))
+        self.assertEqual(idealprank, prank_cmd_1)
+        self.assertEqual(idealmafft, mafft_cmd_1)
+        self.to_be_removed.append(unaligned_seqs)
 
-    # def test_pure_python_plotting(self):
-    #     """
-    #     """
-    #     entropies = []
-    #     with open(self.test_ent_seq, 'r') as entfile:
-    #         for line in entfile:
-    #             entropies.append(float(line.strip()))
-    #     pure_python_plotting(data=entropies, script_name="testplot.R",
-    #                          outdir=self.testdirname, name="Sample",
-    #                          outfile_prefix="test_generated_entropy_plot",
-    #                          DEBUG=True)
+    def test_pure_python_plotting(self):
+        """
+        """
+        entropies = []
+        with open(self.test_ent_seq, 'r') as entfile:
+            for line in entfile:
+                entropies.append(float(line.strip()))
+        pure_python_plotting(data=entropies, script_name="testplot.R",
+                             outdir=self.testdirname, name="Sample",
+                             outfile_prefix="test_generated_entropy_plot",
+                             DEBUG=True)
 
     def tearDown(self):
         """ delete temp files if no errors
