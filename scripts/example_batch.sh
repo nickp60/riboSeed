f617b7a2d6346a0f63d3e13d9696aa326434de3d
nickp60 <nickp60@gmail.com>
2016-12-02 13:19:26 +0000

    edited shell scripts for batching

--- scripts/example_batch.sh
+++ scripts/example_batch.sh
@@ -13,25 +13,25 @@ set -e # exit on error!
 # output scanScaffolds_combined.gb in current directory
 echo 'USAGE: /path/to/genome.gb path/to/genome.fasta path/to/read1 path/to/read2 /path/to/outdir/ n_iterations n_flanking n_cores'
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ -z "$7" ] || [ -z "$8" ] ;
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ -z "$7" ] ;
 then
     echo "All mandatory arguments: genome.gb, genome.fasta, read1, read2, output_dir, iterations, flanking_width, virtenve_exe"
     exit 1
 fi
 echo "example_script.sh PATH: \n $PATH"
-if [ -d "$5" ]; then
+if [ -d "$4" ]; then
     echo "output dir exists!"
     echo "play things safe, make a new directory for results"
     exit 1
 fi
 GB="$1"
-FASTA="$2"
-READ1="$3"
-READ2="$4"
-OUTDIR="$5"
-ITERATIONS="$6"
-FLANK="$7"
-VENVEXE="$8"
+# FASTA="$2"
+READ1="$2"
+READ2="$3"
+OUTDIR="$4"
+ITERATIONS="$5"
+FLANK="$6"
+VENVEXE="$7"
 ## make  dirs
 mkdir ${OUTDIR}
 ## enter virtual environment
@@ -39,6 +39,6 @@ source ${VENVEXE}
 ## RiboSelect
 python3.5 ~/GitHub/riboSeed/riboSeed/riboSelect.py ${GB} -o ${OUTDIR}select/ -v 1
 ## RiboSnag
-python3.5 ~/GitHub/riboSeed/riboSeed/riboSnag.py ${GB} ${OUTDIR}select/riboSelect_grouped_loci.txt -o ${OUTDIR}snag/ -l $FLANK -v 1
+# python3.5 ~/GitHub/riboSeed/riboSeed/riboSnag.py ${GB} ${OUTDIR}select/riboSelect_grouped_loci.txt -o ${OUTDIR}snag/ -l $FLANK -v 1
 # RiboSeed
-python3.5 ~/GitHub/riboSeed/riboSeed/riboSeed.py ${OUTDIR}snag/ -F ${READ1} -R ${READ2} -r ${FASTA} -v 1 -i ${ITERATIONS} -o ${OUTDIR}seed/ -v 1
+python3.5 ~/GitHub/riboSeed/riboSeed/riboSeed.py ${OUTDIR}snag/ ${OUTDIR}select/riboSelect_grouped_loci.txt -F ${READ1} -R ${READ2} -r ${GB} -v 1 -i ${ITERATIONS} -o ${OUTDIR}seed/ -v 1
--- scripts/sge_batch.sh
+++ scripts/sge_batch.sh
@@ -12,20 +12,19 @@
 echo "Hostname: $HOSTNAME"
 echo "Task started `date`"
 echo "if running without qsub:"
-echo "USAGE\n sge_batch.sh genome.gb genome.fasta reads_f.fastq reads_r.fastq /path/to/outdir/ n_iterations, n_flanking, venv_exe"
+echo "USAGE\n sge_batch.sh genome.gb reads_f.fastq reads_r.fastq /path/to/outdir/ n_iterations, n_flanking, venv_exe"
 echo "sge_batch.sh PATH: \n $PATH"
 
 GB="$1"
-FASTA="$2"
-READ1="$3"
-READ2="$4"
-OUTDIR="$5"
-ITERATIONS="$6"
-FLANK="$7"
-VENVEXE="$8"
+READ1="$2"
+READ2="$3"
+OUTDIR="$4"
+ITERATIONS="$5"
+FLANK="$6"
+VENVEXE="$7"
 echo "$PATH"
 # include any commands and/or other
 # bash scripting instructions here
-./scripts/example_batch.sh $GB $FASTA $READ1 $READ2 $OUTDIR $ITERATIONS $FLANK $VENVEXE
+./scripts/example_batch.sh $GB $READ1 $READ2 $OUTDIR $ITERATIONS $FLANK $VENVEXE
 
 echo "Task ended `date`"
